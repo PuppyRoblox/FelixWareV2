17c3ec43c5ad8001bef7c2ecb81de087913692e4
PuppyRoblox <113235599+PuppyRoblox@users.noreply.github.com>
2022-10-17 20:54:16 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,13 +1,26 @@
 -- https://discord.gg/rHFUQksk5f
 
+local request = http and http.request or fluxus and fuxus.request or syn and syn.request or request or http_request
+
 local game_table = {
     [9993529229] = "https://raw.githubusercontent.com/PuppyRoblox/FelixWareV2/main/Main.lua";
     [3101667897] = "https://raw.githubusercontent.com/PuppyRoblox/FelixWareV2/main/Legends%20of%20Speed.lua"
 }
-function joindisco()
-    getgenv().InviteCode = "https://discord.gg/rHFUQksk5f"
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaQLeak/Scripts/main/Discord-Auto-Join.lua"))()
-end
-coroutine.wrap(joindisco)()
+
+request({
+    Url = "http://127.0.0.1:6463/rpc?v=1",
+    Method = "POST",
+    Headers = {
+        ["Content-Type"] = "application/json",
+        ["Origin"] = "https://discord.com"
+    },
+    Body = game:GetService("HttpService"):JSONEncode({
+        cmd = "INVITE_BROWSER",
+        args = {
+            code = "rHFUQksk5f"
+        },
+        nonce = game:GetService("HttpService"):GenerateGUID(false)
+    }),
+ })
 
 loadstring(game:HttpGet(game_table[game.PlaceId]))()
