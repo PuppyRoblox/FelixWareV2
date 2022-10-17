e72399c9ff70648311c7455b8a20cfba9f8d083a
PuppyRoblox <113235599+PuppyRoblox@users.noreply.github.com>
2022-10-17 20:37:31 +0100

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,6 @@
+local game_table = {
+    [9993529229] = "https://raw.githubusercontent.com/PuppyRoblox/FelixWareV2/main/Main.lua";
+    [3101667897] = "https://raw.githubusercontent.com/PuppyRoblox/FelixWareV2/main/Legends%20of%20Speed.lua"
+}
+
+loadstring(game:HttpGet(game_table[game.PlaceId]))()
