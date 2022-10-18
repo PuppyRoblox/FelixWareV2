c088176192683321d9ace0438af562608411d3ff
PuppyRoblox <113235599+PuppyRoblox@users.noreply.github.com>
2022-10-18 19:42:14 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -9,6 +9,10 @@ local vartable = {
     ["ESP_Enabled"] = false;
     ["Cases_Enabled"] = false;
     ["Cases_Case"] = "";
+    ["Kill_All"] = false;
+
+    ["Trigger Bot"] = false;
+    ["Delay"] = 0;
 
     ["Dev_Varabials"] = {
         ["Cases Ava"] = {
@@ -45,9 +49,13 @@ SecureFolder.Name = "Felixware_Secure"
 local HighlightFolder = Instance.new("Folder", SecureFolder);
 HighlightFolder.Name = game:GetService("HttpService"):GenerateGUID(true);
 
+local SecureID_v1 = game:GetService("HttpService"):GenerateGUID(false);
+
 
 local FrameRendered = game:GetService("RunService").RenderStepped;
 local PhysicsRendered = game:GetService("RunService").Heartbeat;
+local UserHandler = game:GetService("VirtualUser");
+local Mouse = game.Players.LocalPlayer:GetMouse();
 
 -- Functions
 local function removeESP()
@@ -59,12 +67,107 @@ local function removeESP()
     end
 end
 
+local function trigger()
+    wait(vartable["Delay"] / 1000)
+    UserHandler:ClickButton1(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+end
+
+local Rage = win:Tab("Rage")
+local Legit = win:Tab("Legit")
 local Visuals = win:Tab("Visuals")
 local Cases = win:Tab("Cases")
 
 local Info = win:Tab("Infomation")
 
+local TriggerBot = Legit:Section("Trigger Bot")
+local KillAll = Rage:Section("Kill All")
 local highlight_esp = Visuals:Section("HighlightESP")
+
+--[[
+
+TriggerBot:Toggle(
+    "Enable",
+    vartable["Trigger Bot"],
+    "Toggle",
+    function(state)
+        vartable["Trigger Bot"] = state
+
+        while true do
+            FrameRendered:Wait()
+            if not vartable["Trigger Bot"] then return end
+
+            for _,v in pairs(game.Players:GetPlayers()) do
+                if v.Character and v ~= game.Players.LocalPlayer and v.Team ~= game.Players.LocalPlayer.Team then
+
+                    if Mouse.Target.Parent == v.Character then
+                        coroutine.wrap(trigger)()
+                    end
+
+                end
+            end
+
+        end
+
+    end
+)
+
+TriggerBot:Slider(
+    "Delay in ms",
+    0,
+    1000,
+    0,
+    1,
+    "Slider",
+    function(state)
+        vartable["Delay"] = state
+    end
+)
+
+]]--
+
+KillAll:Toggle(
+    "Enable",
+    vartable["Kill_All"],
+    "Toggle",
+    function(state)
+        vartable["Kill_All"] = state
+        while true do
+            PhysicsRendered:Wait(3);
+
+            if not vartable["Kill_All"] then return end
+
+            pcall(function()
+                for _,v in pairs(game.Players:GetPlayers()) do
+
+                    if v.Character and v ~= game.Players.LocalPlayer and v.Team ~= game.Players.LocalPlayer.Team then
+    
+                        if v.Character:FindFirstChild("Head") then
+                            local ohInstance1 = v.Character.Head
+                            local ohVector32 = Vector3.new(-874.1176147460938, 982.31982421875, 94.69325256347656)
+                            local ohString3 = "AWP"
+                            local ohNumber4 = 8192
+                            local ohInstance5 = workspace.thegraynotepadv4.Gun
+                            local ohNil6 = nil
+                            local ohNil7 = nil
+                            local ohNumber8 = 1
+                            local ohNil9 = nil
+                            local ohBoolean10 = false
+                            local ohVector311 = Vector3.new(-870.7688598632812, 987.1755981445312, 107.55976104736328)
+                            local ohNumber12 = 100
+                            local ohVector313 = Vector3.new(0, 1, 0)
+                            local ohBoolean14 = true
+    
+                            game:GetService("ReplicatedStorage").Events.EEED:FireServer(ohInstance1, ohVector32, ohString3, ohNumber4, ohInstance5, ohNil6, ohNil7, ohNumber8, ohNil9, ohBoolean10, ohVector311, ohNumber12, ohVector313, ohBoolean14)
+                        end
+    
+                    end
+    
+                end
+            end)
+
+        end
+    end
+)
 
 highlight_esp:Toggle(
     "Enable",
@@ -165,16 +268,16 @@ MainSettings_Cases:Dropdown(
 local ClientInfo_Info = Info:Section("Client Info")
 local fps_info = ClientInfo_Info:Label("FPS: N/A")
 local ping_info = ClientInfo_Info:Label("PING: N/A")
-local userid_info = ClientInfo_Info:Label("UserID: "..game.Players.LocalPlayer.UserId)
-local ClientID_info = ClientInfo_Info:Label("ClientID: "..game:GetService("RbxAnalyticsService"):GetClientId())
+ClientInfo_Info:Label("UserID: "..game.Players.LocalPlayer.UserId)
+ClientInfo_Info:Label("ClientID: "..game:GetService("RbxAnalyticsService"):GetClientId())
 
 local GameInfo_Info = Info:Section("Game Info")
-local serverid_info = GameInfo_Info:Label("ServerID: "..game.JobId)
-local gameid_info = GameInfo_Info:Label("GameID: "..game.PlaceId)
+GameInfo_Info:Label("ServerID: "..game.JobId)
+GameInfo_Info:Label("GameID: "..game.PlaceId)
 
 local ScriptInfo_Info = Info:Section("Script Info")
-local version_info = ScriptInfo_Info:Label("V2.2.0a")
-local seucreid_info = ScriptInfo_Info:Label("SecureID: "..HighlightFolder.Name)
+ScriptInfo_Info:Label("V2.2.2a")
+ScriptInfo_Info:Label("SecureID: "..SecureID_v1)
 local remotesfired_info = ScriptInfo_Info:Label("RemotesFired: 0")
 local idestroy_info = ScriptInfo_Info:Label("Instances Deleted: 0")
 local imade_info = ScriptInfo_Info:Label("Instances Made: 0")
@@ -184,8 +287,10 @@ CreatorInfo_Info:Label("Made by Deducted#1625")
 
 
 while true do
-    wait(1)
+    PhysicsRendered:Wait(0.01)
     remotesfired_info:Set("RemotesFired: "..vartable["Dev_Varabials"]["IRemotes"])
     idestroy_info:Set("Instances Deleted: "..vartable["Dev_Varabials"]["IDestroy"])
     imade_info:Set("Instances Made: "..vartable["Dev_Varabials"]["IMade"])
+    fps_info:Set("FPS: "..workspace:GetRealPhysicsFPS())
+    ping_info:Set("Ping: "..game:GetService("Stats").Network.ServerStatsItem["Data Ping"]:GetValueString())
 end
